e3ceda2388b0f022cf1b4f6bac112b8339d61c8f
Juan Jose Gazzola <devopstobe@gmail.com>
2020-10-14 08:09:53 -0300

    Add variables and assigning values

--- terraform/01-varialbes/main.tf
+++ terraform/01-varialbes/main.tf
@@ -1,53 +1,45 @@
-provider "aws"{
-    region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
 
 # VPC Creation
 
-resource "aws_vpc" "development" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "aws_vpc_definition" {
+  cidr_block = var.aws_vpc_cidr_block
   tags = {
-    Name = "development"
+    Name = var.aws_vpc_name
   }
 }
 
-resource "aws_subnet" "development-az1" {
-  vpc_id     = aws_vpc.development.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "aws_subnet_definition-az1" {
+  vpc_id     = aws_vpc.aws_vpc_definition.id
+  cidr_block = var.aws_subnet_cidr_block
 
   tags = {
-    Name = "app-az1"
+    Name = var.aws_subnet_name
   }
 }
 
 resource "aws_vpc_dhcp_options" "dns_resolver" {
-  domain_name_servers = ["8.8.8.8", "8.8.4.4"]
+  domain_name_servers = var.aws_domain_name_servers
 }
 
 resource "aws_vpc_dhcp_options_association" "dns_resolver" {
-  vpc_id          = aws_vpc.development.id
+  vpc_id          = aws_vpc.aws_vpc_definition.id
   dhcp_options_id = aws_vpc_dhcp_options.dns_resolver.id
 }
 
 resource "aws_security_group" "allow_tls" {
   name        = "allow_remote"
   description = "Allow remote inbound traffic"
-  vpc_id      = aws_vpc.development.id
+  vpc_id      = aws_vpc.aws_vpc_definition.id
 
   ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [aws_vpc.development.cidr_block]
-  }
-
-  ingress {
-    description = "SSH"
-    from_port   = 3389
-    to_port     = 3389
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.development.cidr_block]
+    cidr_blocks = [aws_vpc.aws_vpc_definition.cidr_block]
   }
 
   egress {
---
+++ terraform/01-varialbes/variables.tf
@@ -0,0 +1,27 @@
+variable "aws_vpc_name" {
+  type = string
+}
+
+variable "aws_subnet_name" {
+  type = string
+}
+
+variable "aws_vpc_cidr_block" {
+  type = string
+}
+
+variable "aws_subnet_cidr_block" {
+  type = string
+}
+
+variable "aws_domain_name_servers" {
+  type = list
+}
+
+variable "aws_from_port_ingress" {
+  type = number
+}
+
+variable "aws_to_port_ingress" {
+  type = number
+}
